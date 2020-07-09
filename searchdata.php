6586a5812c1503386ea9a1f2935316e34357e1f4
ray560 <67918608+ray560@users.noreply.github.com>
2020-07-09 12:17:17 +0300

    Add files via upload

--- searchdata.php
+++ searchdata.php
@@ -11,8 +11,8 @@
 	 <div class="container">
 		<form action="" method="POST"
 		
-			<input type="varchar" name="driver_id" placeholder="Enter Driver ID">
-			<input type="varchar" name="driver_id" placeholder="Enter Driver ID">
+			<input type="varchar" name="surname" placeholder="Enter Surname">
+			<input type="varchar" name="surname" placeholder="Enter Surname">
 			<input type="submit" name="search" value="Search by ID">
 		</form>
 		<table border="1px" style="width:600px; line-height:40px; margin-left:10px; margin-top:30px; margin-bottom:100px;">
@@ -33,8 +33,8 @@
 			   
 			   if(isset($_POST['search']))
 			   {
-				   $driver_id = $_POST['driver_id'];
-				   $query = "SELECT * FROM driver WHERE driver_id='driver_id' LIKE";
+				   $surname = $_POST['surname'];
+				   $query = "SELECT * FROM driver WHERE Surname LIKE surname";
 				   $result = mysqli_query($connection, $query) or die("Error: " . mysqli_error($connection));
 				   
 				   while($row = mysqli_fetch_array($result))
